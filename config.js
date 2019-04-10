7d0357322b6d56d0a0350770bb9f540ead73945d
Kousa <jami.kousa@helsinki.fi>
2019-04-10 11:29:45 +0300

    convention over configuration

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports.NODE_ENV = process.env.NODE_ENV || 'development'
 
-module.exports.DB_URL = process.env.DB_URL
+module.exports.DB_URL = process.env.MONGODB_URI
 
 module.exports.PORT = process.env.PORT || 3000
